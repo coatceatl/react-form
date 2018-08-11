977cc410433fdad85e979aa172538116dccc0f93
Oksana Tkachenko <oksana@crystax.net>
2018-08-11 14:44:10 +0300

    changed component form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      email: ''
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('form submitted');
+  }
+
   render() {
     return (
-      <div>form</div>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          placeholder='Email'
+          value={this.state.email}
+        />
+        <button>Send</button>
+      </form>
     )
   }
 }
